29e7ad76d4276c854c631f5a07f133c4b2aada30
MascioSpam <masciospam@openmailbox.org>
2016-05-06 16:19:44 +0200

    [Project] Added a reference to Core in GUI project

---
+++ ShisenSho/ShisenShoGUI/MainWindow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Gtk;
+using ShisenShoCore;
+
+/*
+*    Copyright 2016 Flavio Mascetti - Davide Cortellucci
+*
+*    This file is part of ShisenSho.
+*
+*    ShisenSho is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    ShisenSho is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with ShisenSho.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+public partial class MainWindow: Gtk.Window
+{
+	private Core c;
+	
+	public MainWindow (Core core) : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		this.c = core;
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+}
